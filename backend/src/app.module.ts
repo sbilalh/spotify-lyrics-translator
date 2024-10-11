f8ee8300658361237e2d6be41a09a3ac6f692498
Bilal Hussain <bilal@methodfi.com>
2024-10-10 23:29:51 -0400

    stuff

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { LyricsModule } from './lyrics/lyrics.module';
+import { SpotifyModule } from './spotify/spotify.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, SpotifyModule, LyricsModule],
   controllers: [AppController],
   providers: [AppService],
 })
